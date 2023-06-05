ba48d0981f4d12a0ad6b8e1b8662780201c74c8a
nikand19 <93384362+nikand19@users.noreply.github.com>
2023-06-05 20:37:20 +0530

    Add files via upload

---
+++ Drive/drive.js
@@ -0,0 +1,12 @@
+function modify(){
+    document.getElementById('main').style.fontSize="40px";
+    document.getElementById('main').style.color="red";
+    // document.getElementById('main').style.fontWeight="bold";
+}
+
+function unmodify(){
+    document.getElementById('main').style.fontSize="30px";
+    document.getElementById('main').style.color="black";
+    // document.getElementById('main').style.fontWeight="normal";
+}
+
